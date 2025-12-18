e92a90cff5ef5793846d37a4d7bb431eb0260305
MohamedAmeer91 <ameermohamed91@gmail.com>
2025-12-18 20:18:11 +0530

    Initial React project setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello, React World!</h1>
+      <p>This is custom React component.</p>
+    </div>
+  )
+}
+
+export default App
